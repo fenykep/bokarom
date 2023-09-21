a1aaf3590bfce5ece57b17cbc9c583075ded0e92
fenykep <abel.fenykep@gmail.com>
2023-09-21 17:21:18 +0200

    git proper

---
+++ api.js
@@ -0,0 +1,106 @@
+// N-up,W-right,
+// import { lookupData } from './db.js';
+// const lookupData = require('./data.js');
+
+const lookupData = {
+  "ff": {
+    "hexColor": "#FF5733",
+    "fullName": "John Doe",
+    "email": "john.doe@example.com"
+  },
+  "11": {
+    "hexColor": "#41FF33",
+    "fullName": "Jane Smith",
+    "email": "jane.smith@example.com"
+  },
+  "3c": {
+    "hexColor": "#3344FF",
+    "fullName": "Mary Johnson",
+    "email": "mary.johnson@example.com"
+  },
+  "7a": {
+    "hexColor": "#FF3388",
+    "fullName": "Robert Brown",
+    "email": "robert.brown@example.com"
+  },
+  "b2": {
+    "hexColor": "#AA88FF",
+    "fullName": "Emily Davis",
+    "email": "emily.davis@example.com"
+  }
+}
+
+function hexStringToArray(hexString) {
+    if (hexString.length % 2 !== 0) {
+        throw new Error("Hex string must have an even number of characters.");
+    }
+
+    const wordArray = [];
+
+    for (let i = 0; i < hexString.length; i += 2) {
+        const word = hexString.slice(i, i + 2);
+        wordArray.push(word);
+    }
+
+    return wordArray;
+}
+
+function leftPad(number, width, paddingChar) {
+    const numString = String(number);
+    const padding = Array(Math.max(width - numString.length, 0) + 1).join(paddingChar || '0');
+    return padding + numString;
+}
+
+async function colorOccupied(weekArray) {
+    // const lookupData = await fetchLookupData();
+
+    if (lookupData) {
+        // Access data using keys ('f3', 'c9', etc.)
+        // const key = 'f3';
+
+        weekArray.forEach(function (slot, index) {
+            const paddedNumber = leftPad(index, 3, '0'); // Left-pad to 3 digits with '0'
+            const selector = `#dr${paddedNumber}`;
+            const element = document.querySelector(selector);
+            element.innerText = slot.toUpperCase();
+            if (slot!="00") {
+                const occupantData = lookupData[slot];
+
+                if (occupantData) {
+                    element.setAttribute('nev', occupantData.fullName);
+                    element.setAttribute('email', occupantData.email);
+                    element.style.backgroundColor = occupantData.hexColor;
+                    element.setAttribute('title', occupantData.fullName + " | " + occupantData.email);
+                    // console.log(`Hex Color: ${occupantData.hexColor}`);
+                    // console.log(`Full Name: ${occupantData.fullName}`);
+                    // console.log(`Email: ${occupantData.email}`);
+                } else {
+                    console.error(`Entry with key '${key}' not found.`);
+                }
+                element.classList.add('occupied');
+                console.log('index: '+index+' %40= '+index%40);
+                if (index%40!=39) {
+                    if (weekArray[index+1] == slot){
+                        element.classList.add('Sop');
+                    }
+                }
+                if (index%40!=0) {
+                    if (weekArray[index-1] == slot){
+                        element.classList.add('Nop');
+                    }
+                }
+            }
+        });
+    }
+
+}
+
+//const hexString ="0011223344556677889900aabbccddeeff0011223344556677889900aabbccddeeff0011223344556677889900aabbccddeeff0000000000000000000000000000000000000000000000000000000000000000000000000000011223344556677889000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011223344556677889900aabbccddeeff0011223344556677889900aabbccddeeff00000000000000000000000000000000";
+const hexString ="00111111000000000000000000000000000000000011111111111100000000000000003c3c3c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000ffffffffff000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000111111110000000000000000000000000000000000000000000000000000";
+// "0000000000000000000000000000000000000000000000000000000000000000000000000000011223344556677889000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000112233445566778890000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+// "0011223344556677889900aabbccddeeff00112233445566778899000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000aabbccddeeff000000000000000000000000000000000000000000000000000000000000000000000000000011223344556677889900aabbccddeeff0011223344556677889900120011223344556677889900aabbccddeeff0011223344556677889900aabbccddeeff00000000000000000000000000000000";
+// "0000000000000000000000000000000000000000000011223344556677889900aabbccddeeff0011223344556677889900120011223344556677889900aabbccddeeff00112233445566778000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000089900aabbccddeeff000000000000066778899aabbccddeefff232419234123824600000000000000000000223344556677889900aabbccddeeff00000000000000000000000000000000";
+// "00110022003300440055006600770088009900aabbccddeeff001122334455000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000066778899aabbccddeefff2324192341238246000110022003300440055006600770088009900aabbccddeeff001122334455000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000066778899aabbccddeefff23241923412382460";
+const wordArray = hexStringToArray(hexString);
+console.log(wordArray);
+colorOccupied(wordArray);
---
+++ pygen/timegen.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+# Define the starting time
+start_time = datetime.strptime("8:00", "%H:%M")
+
+text='''
+<tr>                                                                            
+    <td>{}</td>
+    <td id="dr{}">Data</td>
+    <td id="dr{}">Data</td>
+    <td id="dr{}">Data</td>
+    <td id="dr{}">Data</td>
+    <td id="dr{}">Data</td>
+</tr>
+'''
+
+# Create a list to store the incremental instances
+time_instances = []
+counter = 0
+ids=[0,40,80,120, 160]
+finids=["000","000","000","000","000"]
+
+# Generate 40 incremental instances
+for i in range(40):
+    time_instances.append(start_time.strftime("%H:%M"))
+    start_time += timedelta(minutes=15)
+
+# Print the list of time instances
+for time in time_instances:
+    # print(time)
+    for oneid in ids:
+        finids[ids.index(oneid)]=str(oneid+counter).zfill(3)
+    print(text.format(time,*finids))
+    #print(str(counter).zfill(3))
+    counter+=1
+
