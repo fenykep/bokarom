afbea7ade50af430d7452f8a00d8bac625a8ec40
Abel Bodis <bodis@MacBook-149.local>
2023-10-11 18:43:45 +0200

    first clean commit

--- pygen/timegen.py
+++ pygen/timegen.py
@@ -21,9 +21,10 @@ ids=[0,40,80,120, 160]
 finids=["000","000","000","000","000"]
 
 # Generate 40 incremental instances
-for i in range(40):
+# 40*15 or 10*60
+for i in range(10):
     time_instances.append(start_time.strftime("%H:%M"))
-    start_time += timedelta(minutes=15)
+    start_time += timedelta(minutes=60)
 
 # Print the list of time instances
 for time in time_instances:
@@ -32,5 +33,6 @@ for time in time_instances:
         finids[ids.index(oneid)]=str(oneid+counter).zfill(3)
     print(text.format(time,*finids))
     #print(str(counter).zfill(3))
-    counter+=1
+    # also decrease the counter if higher resolution
+    counter+=4
 
