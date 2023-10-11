7dc4ac26f7c53f652b41e84e24ecbd0167219a80
Abel Bodis <bodis@MacBook-149.local>
2023-10-11 18:16:34 +0200

    mac save before pull

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bokarom
+
+
+At the current state run with
+`cargo run` in the root dir.
+It starts a http (Port 3030) and a websockets (Port 8080) server,
+on the http port it serves the "static/" folder.
+
+In that folder there is an old.html, that binds to the 8080 port of the same IP
+it was served from, estabilishes a websockets connection and then updates the UI with the values received from the broadcast channel of the server.
--- pygen/timegen.py
+++ pygen/timegen.py
@@ -19,6 +19,7 @@ ids=[0,40,80,120, 160]
 finids=["000","000","000","000","000"]
 
 # Generate 40 incremental instances
+# Here you can change 40*15 or 10*60 for resolution
 for i in range(10):
     time_instances.append(start_time.strftime("%H:%M"))
     start_time += timedelta(minutes=60)
@@ -30,5 +31,6 @@ for time in time_instances:
         finids[ids.index(oneid)]=str(oneid+counter).zfill(3)
     print(text.format(time,*finids))
     #print(str(counter).zfill(3))
+    # youll also have to change this var for resolution
     counter+=4
 
