00637622a422d29f15ed9960c6b6c213e8f9068c
fenykep <abel.fenykep@gmail.com>
2023-11-01 18:27:45 +0100

    nothing changes

--- static/js/personSearch.js
+++ static/js/personSearch.js
@@ -1,21 +1,26 @@
-const searchBar = document.querySelector('#searchBar');
+const searchBar = document.querySelector("#searchBar");
 
-searchBar.addEventListener("input" , () => {
+searchBar.addEventListener("input", () => {
 	console.log(searchBar.value);
-	document.querySelectorAll('.personBubble').forEach(bubble => {
-		console.log(bubble.getAttribute('name').slice(0,searchBar.value.length));
+	document.querySelectorAll(".personBubble").forEach((bubble) => {
+		console.log(
+			bubble.getAttribute("name").slice(0, searchBar.value.length),
+		);
 		console.log(searchBar.value);
-		if (bubble.getAttribute('name').slice(0,searchBar.value.length) != searchBar.value) {
-			bubble.style.display="none";
+		if (
+			bubble.getAttribute("name").slice(0, searchBar.value.length) !=
+			searchBar.value
+		) {
+			bubble.style.display = "none";
 		} else {
-			bubble.style.display="block";
+			bubble.style.display = "block";
 		}
 	});
 });
 
-document.querySelectorAll('.personBubble').forEach(bubble => {
+document.querySelectorAll(".personBubble").forEach((bubble) => {
 	bubble.addEventListener("click", () => {
 		// window.location.href='personWeek.html?pers='+bubble.getAttribute('name').replace(' ','_');
-		jank('p');
+		jank("p");
 	});
 });
--- static/js/singlePage.js
+++ static/js/singlePage.js
@@ -1,58 +1,67 @@
 const app = {
     currentPage: null,
-    currentView: 'H',
-    currentID: '00',
-    currentWeek: '42',
+    currentView: "H",
+    currentID: "00",
+    currentWeek: "42",
     setPage(page) {
         this.currentPage = page;
-        document.getElementById("content").textContent = "Current Page: " + page;
-    }
+        document.getElementById("content").textContent =
+            "Current Page: " + page;
+    },
 };
 
-function toggleElements(cardModal, personsGrid, roomImage, weekNoSelector, weekTable, searchBar, floorPlan) {
-  const elements = {
-    cardModal: document.getElementById('cardModal'),
-    personsGrid: document.getElementById('personsGrid'),
-    roomImage: document.getElementById('roomImage'),
-    weekNoSelector: document.getElementById('weekNoSelector'),
-    weekTable: document.getElementById('weekTable'),
-    searchBar: document.getElementById('searchBar'),
-    floorPlan: document.getElementsByClassName('floorPlan')[0],
-  };
+function toggleElements(
+    cardModal,
+    personsGrid,
+    roomImage,
+    weekNoSelector,
+    weekTable,
+    searchBar,
+    floorPlan,
+) {
+    const elements = {
+        cardModal: document.getElementById("cardModal"),
+        personsGrid: document.getElementById("personsGrid"),
+        roomImage: document.getElementById("roomImage"),
+        weekNoSelector: document.getElementById("weekNoSelector"),
+        weekTable: document.getElementById("weekTable"),
+        searchBar: document.getElementById("searchBar"),
+        floorPlan: document.getElementsByClassName("floorPlan")[0],
+    };
 
-  // Check the boolean values and toggle the display property of the elements
-  elements.cardModal.style.display = cardModal ? 'grid' : 'none';
-  elements.personsGrid.style.display = personsGrid ? 'flex' : 'none';
-  elements.roomImage.style.display = roomImage ? 'block' : 'none';
-  elements.weekNoSelector.style.display = weekNoSelector ? 'flex' : 'none';
-  elements.weekTable.style.display = weekTable ? 'block' : 'none';
-  elements.searchBar.style.display = searchBar ? 'block' : 'none';
-  elements.floorPlan.style.display = floorPlan ? 'block' : 'none';
+    // Check the boolean values and toggle the display property of the elements
+    elements.cardModal.style.display = cardModal ? "grid" : "none";
+    elements.personsGrid.style.display = personsGrid ? "flex" : "none";
+    elements.roomImage.style.display = roomImage ? "block" : "none";
+    elements.weekNoSelector.style.display = weekNoSelector ? "flex" : "none";
+    elements.weekTable.style.display = weekTable ? "block" : "none";
+    elements.searchBar.style.display = searchBar ? "block" : "none";
+    elements.floorPlan.style.display = floorPlan ? "block" : "none";
 }
 
-toggleElements(false, false, false, false, false, false, false); // reset
+toggleElements(0, 0, 0, 0, 0, 0, 0); // reset
 
-function jank(urlChar){
-    switch(urlChar){
-        case 'f':
-            toggleElements(true, false, false, false, false, false, true); // floorView
-        break;
-        case 's':
-            toggleElements(false, true, false, false, false, true, false); // searchView
-        break;
-        case 'r':
-            toggleElements(false, false, true, true, true, false, false); // roomView
-        break;
-        case 'p':
-            toggleElements(true, false, false, true, true, false, false); // personView
-        break;
+function jank(urlChar) {
+    switch (urlChar) {
+        case "f":
+            toggleElements(1, 0, 0, 0, 0, 0, 1); // floorView
+            break;
+        case "s":
+            toggleElements(0, 1, 0, 0, 0, 1, 0); // searchView
+            break;
+        case "r":
+            toggleElements(0, 0, 1, 1, 1, 0, 0); // roomView
+            break;
+        case "p":
+            toggleElements(1, 0, 0, 1, 1, 0, 0); // personView
+            break;
         default:
-            toggleElements(false, false, false, false, false, false, false); // reset        
+            toggleElements(0, 0, 0, 0, 0, 0, 0); // reset
     }
 }
 
 window.history.pushState(null, null, window.location.href);
 window.onpopstate = function () {
     window.history.go(1);
-    jank('f');
+    jank("f");
 };
