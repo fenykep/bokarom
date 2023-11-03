b19a199666cce6fa93d744539016a2a2bbebdf27
fenykep <abel.fenykep@gmail.com>
2023-11-03 17:51:04 +0100

    new pictures, preps to integrate

--- static/js/weekSwitcher.js
+++ static/js/weekSwitcher.js
@@ -1,9 +1,11 @@
-const weekNo = document.querySelector("#weekNoSelector div");
+const weekNoText = document.querySelector("#weekNoSelector div");
 
 function weekSwitcher(forward) {
-	var disassembledWeekNo = weekNo.innerText.split(" ");
-	var wn = Number(disassembledWeekNo[1]);
+	var disassembledWeekNoText = weekNoText.innerText.split(" ");
+	var wn = Number(disassembledWeekNoText[1]);
+	wn = wn + (forward ? wn<52?1:0 : wn>1?-1:0);
 	// messy double-ternary but its such a small function
-	var newWeekNo = disassembledWeekNo[0] + " " + (wn + (forward ? wn<52?1:0 : wn>1?-1:0));
-	weekNo.innerText = newWeekNo;
+	var newWeekNoText = disassembledWeekNoText[0] + " " + (wn);
+	weekNoText.innerText = newWeekNoText;
+	console.log('Change weekNo to '+wn);
 }
